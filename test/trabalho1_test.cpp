01eda943f01c7cf6d07d99f6eecf242d5ddccb74
Tiago Melo Juca <contact@tiagomelojuca.com>
2025-10-19 14:40:22 -0300

    mais casos de teste

--- test/trabalho1_test.cpp
+++ test/trabalho1_test.cpp
@@ -6,6 +6,11 @@ TEST(trabalho1_test, deve_ser_capaz_calcular_arvore)
 {
     const double erro = 0.1;
     {
+        // f(x) nao existe
+        TArvoreSintatica arvore { nullptr };
+        EXPECT_NEAR(arvore.Resolve(999), 0.0, erro);
+    }
+    {
         // f(x) = 5.0
         TArvoreSintatica arvore { new TNohConstante { 5.0 } };
         EXPECT_NEAR(arvore.Resolve(999), 5.0, erro);
@@ -15,6 +20,54 @@ TEST(trabalho1_test, deve_ser_capaz_calcular_arvore)
         TArvoreSintatica arvore { new TNohLiteral };
         EXPECT_NEAR(arvore.Resolve(123), 123.0, erro);
         EXPECT_NEAR(arvore.Resolve(999), 999.0, erro);
+    }
+    {
+        // f(x) = sen(x)
+        TArvoreSintatica arvore {
+            new TNohOperacaoUnaria { TNohOperacaoUnaria::EOperacao::SENO, new TNohLiteral }
+        };
+        EXPECT_NEAR(arvore.Resolve(0.0), 0.0, erro);
+        EXPECT_NEAR(arvore.Resolve(0.785), 0.71, erro);
+        EXPECT_NEAR(arvore.Resolve(1.57), 1.0, erro);
+    }
+    {
+        // f(x) = x + 7
+        TArvoreSintatica arvore {
+            new TNohOperacaoBinaria {
+                TNohOperacaoBinaria::EOperacao::SOMA,
+                new TNohLiteral,
+                new TNohConstante { 7.0 }
+            }
+        };
+        EXPECT_NEAR(arvore.Resolve(0.0), 7.0, erro);
+        EXPECT_NEAR(arvore.Resolve(2.0), 9.0, erro);
+    }
+    {
+        // f(x) = 3x^2 * sen(x) + 7
+        auto raiz = new TNohOperacaoBinaria {
+            TNohOperacaoBinaria::EOperacao::SOMA,
+            new TNohOperacaoBinaria {
+                TNohOperacaoBinaria::EOperacao::MULTIPLICACAO,
+                new TNohOperacaoBinaria {
+                    TNohOperacaoBinaria::EOperacao::MULTIPLICACAO,
+                    new TNohConstante { 3.0 },
+                    new TNohOperacaoBinaria {
+                        TNohOperacaoBinaria::EOperacao::EXPONENCIAL,
+                        new TNohLiteral,
+                        new TNohConstante { 2.0 }
+                    }
+                },
+                new TNohOperacaoUnaria {
+                    TNohOperacaoUnaria::EOperacao::SENO,
+                    new TNohLiteral
+                }
+            },
+            new TNohConstante { 7.0 }
+        };
+
+        TArvoreSintatica arvore { raiz };
+        EXPECT_NEAR(arvore.Resolve(2.0), 17.91, erro);
+        EXPECT_NEAR(arvore.Resolve(3.0), 10.81, erro);
     }
     {
         // f(x) = 3x^2 * (sen(x) + 7)
