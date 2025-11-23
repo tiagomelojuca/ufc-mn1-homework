459be515476475068f17d4d7ba132410bf5c4e1f
Tiago Melo Juca <contact@tiagomelojuca.com>
2025-11-23 18:17:17 -0300

    wip flexibilizar entradas

--- src/trabalho1/TCalculoOscilacoesSismicas.h
+++ src/trabalho1/TCalculoOscilacoesSismicas.h
@@ -19,17 +19,24 @@ class TCalculoOscilacoesSismicas
 public:
     enum class EMetodoCalculo { NEWTON_RAPHSON, NEWTON_MODIFICADO, SECANTE };
 
+    struct TEntradaCalculo
+    {
+        double a;
+        double x0;
+        double err;
+    };
+
     TCalculoOscilacoesSismicas() = default;
 
-    std::string Sintetiza(double a) const
+    std::string Sintetiza(TEntradaCalculo e) const
     {
         std::stringstream ss;
 
-        const double dNR = Calcula(EMetodoCalculo::NEWTON_RAPHSON, a);
-        const double dNM = Calcula(EMetodoCalculo::NEWTON_MODIFICADO, a);
-        const double dSc = Calcula(EMetodoCalculo::SECANTE, a);
+        const double dNR = Calcula(EMetodoCalculo::NEWTON_RAPHSON, e);
+        const double dNM = Calcula(EMetodoCalculo::NEWTON_MODIFICADO, e);
+        const double dSc = Calcula(EMetodoCalculo::SECANTE, e);
 
-        ss << "> " << GeraExpressaoDinamicamente(a) << std::endl
+        ss << "> " << GeraExpressaoDinamicamente(e.a) << std::endl
            << "    NEWTON-RAPHSON    : " << dNR << std::endl
            << "    NEWTON-MODIFICADO : " << dNM << std::endl
            << "    SECANTE           : " << dSc << std::endl;
@@ -39,34 +46,33 @@ public:
 
     std::string Sintetiza() const
     {
-        std::vector<double> entradas;
-        entradas.push_back(1.0);
-        entradas.push_back(2.0);
-        entradas.push_back(3.0);
+        std::vector<TEntradaCalculo> entradas;
+        entradas.push_back({ 1.0, 0.5, 0.0001 });
+        entradas.push_back({ 2.0, 1.2, 0.0001 });
+        entradas.push_back({ 3.0, 0.5, 0.0001 });
 
         std::stringstream ss;
+        ss << "| " << std::right
+            << std::setw(10) << std::fixed << std::setprecision(10) << "a" << " | "
+            << std::setw(10) << std::fixed << std::setprecision(10) << "d0" << " | "
+            << std::setw(10) << std::fixed << std::setprecision(10) << "d1" << " | "
+            << std::setw(10) << std::fixed << std::setprecision(10) << "err" << " | "
+            << std::setw(10) << std::fixed << std::setprecision(10) << "d_nr" << " | "
+            << std::setw(10) << std::fixed << std::setprecision(10) << "d_nm" << " | "
+            << std::setw(10) << std::fixed << std::setprecision(10) << "d_sc" << " |"
+            << std::endl;
 
-            ss << "| " << std::right
-               << std::setw(10) << std::fixed << std::setprecision(10) << "a" << " | "
-               << std::setw(10) << std::fixed << std::setprecision(10) << "d0" << " | "
-               << std::setw(10) << std::fixed << std::setprecision(10) << "d1" << " | "
-               << std::setw(10) << std::fixed << std::setprecision(10) << "err" << " | "
-               << std::setw(10) << std::fixed << std::setprecision(10) << "d_nr" << " | "
-               << std::setw(10) << std::fixed << std::setprecision(10) << "d_nm" << " | "
-               << std::setw(10) << std::fixed << std::setprecision(10) << "d_sc" << " |"
-               << std::endl;
-
-        for (double a : entradas)
+        for (TEntradaCalculo e : entradas)
         {
-            const double dNR = Calcula(EMetodoCalculo::NEWTON_RAPHSON, a);
-            const double dNM = Calcula(EMetodoCalculo::NEWTON_MODIFICADO, a);
-            const double dSc = Calcula(EMetodoCalculo::SECANTE, a);
+            const double dNR = Calcula(EMetodoCalculo::NEWTON_RAPHSON, e);
+            const double dNM = Calcula(EMetodoCalculo::NEWTON_MODIFICADO, e);
+            const double dSc = Calcula(EMetodoCalculo::SECANTE, e);
 
             ss << "| " << std::right
-               << std::setw(8) << std::fixed << std::setprecision(8) << a << " | "
-               << std::setw(8) << std::fixed << std::setprecision(8) << 0.5 << " | "
-               << std::setw(8) << std::fixed << std::setprecision(8) << 0.6 << " | "
-               << std::setw(8) << std::fixed << std::setprecision(8) << 0.0001 << " | "
+               << std::setw(8) << std::fixed << std::setprecision(8) << e.a << " | "
+               << std::setw(8) << std::fixed << std::setprecision(8) << e.x0 << " | "
+               << std::setw(8) << std::fixed << std::setprecision(8) << EstimaX1(e.x0) << " | "
+               << std::setw(8) << std::fixed << std::setprecision(8) << e.err << " | "
                << std::setw(8) << std::fixed << std::setprecision(8) << dNR << " | "
                << std::setw(8) << std::fixed << std::setprecision(8) << dNM << " | "
                << std::setw(8) << std::fixed << std::setprecision(8) << dSc << " |"
@@ -76,19 +82,28 @@ public:
         return ss.str();
     }
 
-    double Calcula(EMetodoCalculo metodo, double a) const
+private:
+    double Calcula(EMetodoCalculo metodo, TEntradaCalculo entrada) const
     {
-        const double x0 = 0.5;
-        const double x1 = 0.6;
-        const double errAdm = 0.0001;
+        const double x1 = EstimaX1(entrada.x0);
 
+        return Calcula(metodo, entrada.a, entrada.x0, x1, entrada.err);
+    }
+
+    double Calcula(
+        EMetodoCalculo metodo,
+        double a,
+        double x0,
+        double x1,
+        double errAdm
+    ) const
+    {
         const std::string f = GeraExpressaoDinamicamente(a);
         const std::string f_ = GeraDerivadaDinamicamente(a);
 
         return FabricaCalculo(metodo, f, f_, x0, x1, errAdm)->Busca();
     }
 
-private:
     std::unique_ptr<TCalculoRaizes> FabricaCalculo(
         EMetodoCalculo metodo,
         const std::string& f,
@@ -124,6 +139,11 @@ private:
     std::string GeraDerivadaDinamicamente(double a) const
     {
         return FuncoesGerais::InstanciaModelo(modeloDerivadaFuncaoAplicacaoImagem, std::to_string(a).c_str());
+    }
+
+    double EstimaX1(double x0, double delta = 0.1) const
+    {
+        return x0 + delta;
     }
 
     static constexpr const char* modeloFuncaoOriginal = "f(d) = $*e^d - 4*d^2";
