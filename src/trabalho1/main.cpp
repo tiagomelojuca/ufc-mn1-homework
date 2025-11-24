6e9d80598e61a3837ed3c6e29bdcbf08cf51cafa
Tiago Melo Juca <contact@tiagomelojuca.com>
2025-11-23 23:05:46 -0300

    interface cli generoca

--- src/trabalho1/main.cpp
+++ src/trabalho1/main.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
 
+#include "TImpressoraCalculoOscilacoesSismicas.h"
+
 int main()
 {
-    std::cout << "Trabalho 1" << '\n';
+    TImpressoraCalculoOscilacoesSismicas impressora;
+    
+    double err;
+    std::cout << "Digite a precisao desejada: ";
+    std::cin >> err;
+
+    int i = 1;
+
+    double a;
+    std::cout << "Digite o i-esimo valor de a, ou 0 para finalizar" << '\n';
+    do {
+        std::cout << "    a" << i << ": ";
+        std::cin >> a;
+
+        if (a != 0.0)
+        {
+            impressora.Adiciona(a, 0.0, err);
+            i++;
+        }
+    } while (a != 0.0);
+
+    std::cout << impressora.Sintetiza();
 
     return 0;
 }
