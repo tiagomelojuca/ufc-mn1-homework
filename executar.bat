5770404f3606ae5fa4b638a5d7961a1d55405334
Tiago Melo Juca <contact@tiagomelojuca.com>
2026-01-02 16:53:21 -0300

    boilerplate trabalho2

--- executar.bat
+++ executar.bat
@@ -1,5 +1,5 @@
 @echo off
 set "SCRIPT_DIR=%~dp0"
 if "%SCRIPT_DIR:~-1%"=="\" set "SCRIPT_DIR=%SCRIPT_DIR:~0,-1%"
-set "EXE_PATH=%SCRIPT_DIR%\out\exeobj_cmake\trabalho1.exe"
+set "EXE_PATH=%SCRIPT_DIR%\out\exeobj_cmake\trabalho2.exe"
 "%EXE_PATH%" %*
