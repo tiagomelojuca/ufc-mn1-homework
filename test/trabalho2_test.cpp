9ee2835f0172961f3b98660da196d2b01df2fb22
Tiago Melo Juca <contact@tiagomelojuca.com>
2026-01-17 13:38:42 -0300

    desabilitar testes nao convergem por criterio linhas

--- test/trabalho2_test.cpp
+++ test/trabalho2_test.cpp
@@ -25,51 +25,51 @@ TEST(trabalho2_test, deve_ser_capaz_de_calcular_inversa)
 TEST(trabalho2_test, deve_ser_capaz_de_resolver_sistema)
 {
     const float tol = 0.01f;
-    {
-        TMatriz A(3u, 3u, { { 3.0, 2.0,  4.0 },
-                            { 1.0, 1.0,  2.0 },
-                            { 4.0, 3.0, -2.0 } });
-        TMatriz b(3u, 1u, { { 1.0 },
-                            { 2.0 },
-                            { 3.0 } });
-        TMatriz xEsperado(3u, 1u, { { -3.0 },
-                                    {  5.0 },
-                                    {  0.0 } });
+    // {
+    //     TMatriz A(3u, 3u, { { 3.0, 2.0,  4.0 },
+    //                         { 1.0, 1.0,  2.0 },
+    //                         { 4.0, 3.0, -2.0 } });
+    //     TMatriz b(3u, 1u, { { 1.0 },
+    //                         { 2.0 },
+    //                         { 3.0 } });
+    //     TMatriz xEsperado(3u, 1u, { { -3.0 },
+    //                                 {  5.0 },
+    //                                 {  0.0 } });
         
-        TSistema s(A, b);
-        EXPECT_TRUE(s.Resolve(TSistema::EMetodo::GAUSS_JACOBI).Igual(xEsperado, tol));
-        EXPECT_TRUE(s.Resolve(TSistema::EMetodo::GAUSS_SEIDEL).Igual(xEsperado, tol));
-    }
-    {
-        TMatriz A(3u, 3u, { {  1.0, -3.0,  2.0 },
-                            { -2.0,  8.0, -1.0 },
-                            {  4.0, -6.0,  5.0 } });
-        TMatriz b(3u, 1u, { {  11.0 },
-                            { -15.0 },
-                            {  29.0 } });
-        TMatriz xEsperado(3u, 1u, { {  2.0 },
-                                    { -1.0 },
-                                    {  3.0 } });
+    //     TSistema s(A, b);
+    //     EXPECT_TRUE(s.Resolve(TSistema::EMetodo::GAUSS_JACOBI, 0.05).Igual(xEsperado, tol));
+    //     EXPECT_TRUE(s.Resolve(TSistema::EMetodo::GAUSS_SEIDEL, 0.05).Igual(xEsperado, tol));
+    // }
+    // {
+    //     TMatriz A(3u, 3u, { {  1.0, -3.0,  2.0 },
+    //                         { -2.0,  8.0, -1.0 },
+    //                         {  4.0, -6.0,  5.0 } });
+    //     TMatriz b(3u, 1u, { {  11.0 },
+    //                         { -15.0 },
+    //                         {  29.0 } });
+    //     TMatriz xEsperado(3u, 1u, { {  2.0 },
+    //                                 { -1.0 },
+    //                                 {  3.0 } });
         
-        TSistema s(A, b);
-        EXPECT_TRUE(s.Resolve(TSistema::EMetodo::GAUSS_JACOBI).Igual(xEsperado, tol));
-        EXPECT_TRUE(s.Resolve(TSistema::EMetodo::GAUSS_SEIDEL).Igual(xEsperado, tol));
-    }
-    {
-        TMatriz A(3u, 3u, { { 3.0, -4.0,  1.0 },
-                            { 1.0,  2.0,  2.0 },
-                            { 4.0,  0.0, -3.0 } });
-        TMatriz b(3u, 1u, { {  9.0 },
-                            {  3.0 },
-                            { -2.0 } });
-        TMatriz xEsperado(3u, 1u, { {  1.0 },
-                                    { -1.0 },
-                                    {  2.0 } });
+    //     TSistema s(A, b);
+    //     EXPECT_TRUE(s.Resolve(TSistema::EMetodo::GAUSS_JACOBI, 0.05).Igual(xEsperado, tol));
+    //     EXPECT_TRUE(s.Resolve(TSistema::EMetodo::GAUSS_SEIDEL, 0.05).Igual(xEsperado, tol));
+    // }
+    // {
+    //     TMatriz A(3u, 3u, { { 3.0, -4.0,  1.0 },
+    //                         { 1.0,  2.0,  2.0 },
+    //                         { 4.0,  0.0, -3.0 } });
+    //     TMatriz b(3u, 1u, { {  9.0 },
+    //                         {  3.0 },
+    //                         { -2.0 } });
+    //     TMatriz xEsperado(3u, 1u, { {  1.0 },
+    //                                 { -1.0 },
+    //                                 {  2.0 } });
 
-        TSistema s(A, b);
-        EXPECT_TRUE(s.Resolve(TSistema::EMetodo::GAUSS_JACOBI).Igual(xEsperado, tol));
-        EXPECT_TRUE(s.Resolve(TSistema::EMetodo::GAUSS_SEIDEL).Igual(xEsperado, tol));
-    }
+    //     TSistema s(A, b);
+    //     EXPECT_TRUE(s.Resolve(TSistema::EMetodo::GAUSS_JACOBI, 0.05).Igual(xEsperado, tol));
+    //     EXPECT_TRUE(s.Resolve(TSistema::EMetodo::GAUSS_SEIDEL, 0.05).Igual(xEsperado, tol));
+    // }
     {
         TMatriz A(3u, 3u, { { 5.0, 3.0, 1.0 },
                             { 5.0, 6.0, 1.0 },
@@ -82,8 +82,8 @@ TEST(trabalho2_test, deve_ser_capaz_de_resolver_sistema)
                                     {  5.0/34.0 } });
         
         TSistema s(A, b);
-        EXPECT_TRUE(s.Resolve(TSistema::EMetodo::GAUSS_JACOBI).Igual(xEsperado, tol));
-        EXPECT_TRUE(s.Resolve(TSistema::EMetodo::GAUSS_SEIDEL).Igual(xEsperado, tol));
+        EXPECT_TRUE(s.Resolve(TSistema::EMetodo::GAUSS_JACOBI, 0.01).Igual(xEsperado, tol));
+        EXPECT_TRUE(s.Resolve(TSistema::EMetodo::GAUSS_SEIDEL, 0.01).Igual(xEsperado, tol));
     }
 }
 
