ac2ed9c624851cb76d83df2cc54b20f473d5a172
Tiago Melo Juca <contact@tiagomelojuca.com>
2026-01-17 13:39:54 -0300

    impl gauss jacobi

--- src/trabalho2/TSistema.h
+++ src/trabalho2/TSistema.h
@@ -1,6 +1,8 @@
 #ifndef TSISTEMA_H_
 #define TSISTEMA_H_
 
+#include <cmath>
+
 #include "TMatriz.h"
 
 // ------------------------------------------------------------------------------------------------
@@ -16,28 +18,74 @@ public:
 
     TSistema(const TMatriz& A, const TMatriz& b) : _A(A), _b(b) {}
 
-    TMatriz Resolve(EMetodo m) const
+    TMatriz Resolve(EMetodo m, double errAdm) const
     {
         return m == EMetodo::GAUSS_JACOBI
-            ? ResolvePorGaussJacobi()
-            : ResolvePorGaussSeidel();
+            ? ResolvePorGaussJacobi(errAdm)
+            : ResolvePorGaussSeidel(errAdm);
     }
 
 private:
-    TMatriz ResolvePorGaussJacobi() const
+    TMatriz ResolvePorGaussJacobi(double errAdm) const
+    {
+        TMatriz C = _A;
+        for (int l = 1; l <= C.Linhas(); l++)
+        {
+            for (int c = 1; c <= C.Colunas(); c++)
+            {
+                double c_lc = c == l ? 0.0 : C.Valor(l, c) / -_A.Valor(l, l);
+                C.Valor(l, c, c_lc);
+            }
+        }
+
+        TMatriz g = _b;
+        for (int l = 1; l <= g.Linhas(); l++)
+        {
+            double g_l1 = g.Valor(l, 1) / _A.Valor(l, l);
+            g.Valor(l, 1, g_l1);
+        }
+
+        uint8_t iter = 0u;
+        double err = std::numeric_limits<double>::max();
+        TMatriz xAnt { g.Linhas(), g.Colunas() };
+        TMatriz x = g; // costuma-se usar x0 como o proprio g, economiza 1 iteracao
+        while (err > errAdm && iter < _maxIter)
+        {
+            xAnt = x;
+            x = C.Produto(x).Soma(g);
+            err = ArgMax(x.Soma(xAnt.Produto(-1.0))) / ArgMax(x);
+            iter++;
+        }
+
+        return x;
+    }
+
+    TMatriz ResolvePorGaussSeidel(double errAdm) const
     {
         // ToDo
         return TMatriz(0u, 0u);
     }
 
-    TMatriz ResolvePorGaussSeidel() const
+    double ArgMax(const TMatriz& M) const
     {
-        // ToDo
-        return TMatriz(0u, 0u);
+        double max = -1.0;
+
+        for (int l = 1; l <= M.Linhas(); l++)
+        {
+            const double m = fabs(M.Valor(l, 1));
+            if (m > max)
+            {
+                max = m;
+            }
+        }
+
+        return max;
     }
 
     TMatriz _A;
     TMatriz _b;
+
+    uint8_t _maxIter = 255u;
 };
 
 // ------------------------------------------------------------------------------------------------
