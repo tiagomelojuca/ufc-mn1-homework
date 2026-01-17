2cbb77e1d630655188cf54c23d22955de28635dc
Tiago Melo Juca <contact@tiagomelojuca.com>
2026-01-17 16:53:04 -0300

    impl executavel principal

--- src/trabalho2/main.cpp
+++ src/trabalho2/main.cpp
@@ -1,8 +1,31 @@
-#include <iostream>
+#include "TOrquestradorES.h"
+
+// ------------------------------------------------------------------------------------------------
+#define SEM_ERRO 0
+// ------------------------------------------------------------------------------------------------
+
+TOrquestradorES::TDadosSaida Calcula(const TOrquestradorES::TDadosEntrada& dadosEntrada)
+{
+    const TMatriz& A = dadosEntrada.sis.MatrizCoeficientes();
+    const TSistema::EMetodo metodo = dadosEntrada.met;
+    const double errAdm = dadosEntrada.err;
+
+    const TMatriz invA = TSistema::Inversa(A, metodo, errAdm);
+    const TMatriz& b = dadosEntrada.sis.VetorTermosIndependentes();
+    TMatriz x = invA.Produto(b);
+
+    return { invA, x };
+}
+
+// ------------------------------------------------------------------------------------------------
 
 int main()
 {
-    std::cout << "Hello, trabalho2" << std::endl;
+    const auto dadosEntrada = TOrquestradorES::SolicitaDadosEntrada();
+    const auto dadosSaida = Calcula(dadosEntrada);
+    TOrquestradorES::Imprime(dadosSaida);
 
-    return 0;
+    return SEM_ERRO;
 }
+
+// ------------------------------------------------------------------------------------------------
