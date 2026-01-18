149ae57016765ef46a11ebd768d490705622809b
Tiago Melo Juca <contact@tiagomelojuca.com>
2026-01-18 18:19:13 -0300

    compilar em modo release

--- compilar.bat
+++ compilar.bat
@@ -1,2 +1,7 @@
-cmake -S . -B out -DBUILD_UNIT_TESTS=OFF
-cmake --build out --target install
+@REM Para GCC,  precisamos setar a flag na geracao do projeto
+@REM Para MSVC, precisamos passar a flag "config" na construcao, pois
+@REM ele ignora a variavel passada na geracao (eh um generator multiconfig)
+@REM https://stackoverflow.com/
+@REM /questions/24460486/cmake-build-type-is-not-being-used-in-cmakelists-txt
+cmake -S . -B out -DCMAKE_BUILD_TYPE=RelWithDebInfo -DBUILD_UNIT_TESTS=OFF
+cmake --build out --config RelWithDebInfo --target install
